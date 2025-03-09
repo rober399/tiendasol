e876dc2fc146dd176c75e144ad81f985a9bff037
Roberto <roberore99@gmail.com>
2025-03-08 20:07:31 -0600

    Cambios en app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for,session, flash
+from flask import Flask, flash, render_template, request, session #redirect, #url_for
 import mysql.connector
 from datetime import datetime
-import bcrypt
+#import bcrypt 
 
 app = Flask(__name__)
 app.secret_key = 'mysecretkey'
